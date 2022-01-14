55f94cf6d91b692269f3e7cbdab13d48aa20a2f7
Mechetiuc Denisa <91954011+denisaMechetiuc@users.noreply.github.com>
2022-01-14 19:01:44 +0200

    Add files via upload

---
+++ Data/IRestService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Mechetiuc_Denisa_12.Models;
+
+namespace Mechetiuc_Denisa_12.Data
+{
+    public interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+        Task DeleteShopListAsync(int id);
+
+    }
+}
