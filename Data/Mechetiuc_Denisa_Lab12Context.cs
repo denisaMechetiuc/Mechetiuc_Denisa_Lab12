230c4e83dddfeeac4251e9874e8eb2bf9565c73a
DENISA-NASTACA MECHETIUC <denisa.mechetiuc@stud.ubbcluj.ro>
2022-01-14 18:58:22 +0200

    Add project files.

---
+++ Data/Mechetiuc_Denisa_Lab12Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Mechetiuc_Denisa_Lab12.Models;
+
+namespace Mechetiuc_Denisa_Lab12.Data
+{
+    public class Mechetiuc_Denisa_Lab12Context : DbContext
+    {
+        public Mechetiuc_Denisa_Lab12Context (DbContextOptions<Mechetiuc_Denisa_Lab12Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Mechetiuc_Denisa_Lab12.Models.ShopList> ShopList { get; set; }
+    }
+}
