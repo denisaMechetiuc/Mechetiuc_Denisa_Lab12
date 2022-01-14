2c09064dbd641913ca1bf704a73014378800c055
Mechetiuc Denisa <91954011+denisaMechetiuc@users.noreply.github.com>
2022-01-14 19:07:36 +0200

    Add files via upload

---
+++ ListPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mechetiuc_Denisa_12.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Mechetiuc_Denisa_12
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListPage : ContentPage
+    {
+        public ListPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            var slist = (ShopList)BindingContext;
+            slist.Date = DateTime.UtcNow;
+            await App.Database.SaveShopListAsync(slist);
+            await Navigation.PopAsync();
+        }
+        async void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            var slist = (ShopList)BindingContext;
+            await App.Database.DeleteShopListAsync(slist);
+            await Navigation.PopAsync();
+        }
+    }
+}
